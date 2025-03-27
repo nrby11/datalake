b7b45f7ac9b2c6458d20968e9b3fe361336f83ee
nirbhaykumar <nrby11@gmail.com>
2025-03-27 09:35:28 +0530

    Refactor Spark job configuration and monitoring scripts for improved clarity and functionality

--- src/modules/transformation/log_transformation.py
+++ src/modules/transformation/log_transformation.py
@@ -5,7 +5,7 @@ from pyspark.sql.functions import (col, date_format, udf,
                                    weekofyear)
 from pyspark.sql.types import StringType
 
-from config import Config
+from src.config.config import Config
 
 logger = logging.getLogger(__name__)
 
